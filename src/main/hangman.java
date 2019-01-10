400a810692f541e3e444b8953c053d44c9f486c2
lakshaydaver <lakshay.daver_ccv17@gla.ac.in>
2019-01-10 15:02:54 +0530

    add bufferedreader for input

--- src/main/hangman.java
+++ src/main/hangman.java
@@ -1,9 +1,23 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class hangman {
 
 
-    
+    public static void main(String[] args) {
+        System.out.println("Enter the secret word");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuffer word; //its immutable
+        try {
+            word = new StringBuffer(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 
